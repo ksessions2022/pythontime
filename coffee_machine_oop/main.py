75bb678678b6e0b39a588274e7d81ca65e42d3f8
Kyle <k20sesh@gmail.com>
2023-06-27 20:29:08 -0600

    classes

--- coffee_machine_oop/main.py
+++ coffee_machine_oop/main.py
@@ -6,12 +6,19 @@ from money_machine import MoneyMachine
 
 coffee_machine = CoffeeMaker()
 my_menu = Menu()
+atm = MoneyMachine()
 
-
-user_request = str.lower(input(f"What would you like? ({my_menu.get_items()}): "))
-requested_item = (my_menu.find_drink(user_request))
-# coffee_machine.report()
-if coffee_machine.is_resource_sufficient(requested_item):
-    print("yay")
-else:
-    print("nay")
+while True:
+    user_request = str.lower(input(f"What would you like? ({my_menu.get_items()}): "))
+    if user_request == "report":
+        coffee_machine.report()
+        atm.report()
+    elif user_request == "off":
+        exit()
+    else:
+        requested_item = (my_menu.find_drink(user_request))
+        if coffee_machine.is_resource_sufficient(requested_item):
+            if atm.make_payment(requested_item.cost):
+                coffee_machine.make_coffee(requested_item)
+        else:
+            print("Not enough resources")
