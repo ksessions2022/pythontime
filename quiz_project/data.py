9611a62a2b1faf4556c8244f0ccf7e39bce7a8e7
Kyle <k20sesh@gmail.com>
2023-06-30 15:16:38 -0600

    quiz project

---
+++ quiz_project/data.py
@@ -0,0 +1,71 @@
+question_data = [
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "The HTML5 standard was published in 2014.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "The first computer bug was formed by faulty wires.",
+            "correct_answer": "False",
+            "incorrect_answers": ["True"]
+        },
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "The very first recorded computer bug was a moth found inside a Harvard Mark II computer.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+            "correct_answer": "False",
+            "incorrect_answers": ["True"]
+        },
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "Windows NT is a monolithic kernel.",
+            "correct_answer": "False",
+            "incorrect_answers": ["True"]
+        },
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "The first dual-core CPU was the Intel Pentium D.",
+            "correct_answer": "False",
+            "incorrect_answers": ["True"]
+        },
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+            "correct_answer": "False",
+            "incorrect_answers": ["True"]
+        },
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "It's not possible to format a write-protected DVD-R Hard Disk.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "Android versions are named in alphabetical order.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]},
+        {
+            "category": "Science: Computers",
+            "type": "boolean", "difficulty": "medium",
+            "question": "A Boolean value of 0 represents which of these words?",
+            "correct_answer": "False",
+            "incorrect_answers": ["True"]
+        }
+]
---
+++ quiz_project/main.py
@@ -0,0 +1,23 @@
+# imports from all the other files in project
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+# makes a blank list for all the questions
+question_bank = []
+
+# for each question in question_model.py
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    # Takes created variables above and inputs the question and answer into a question bank
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+# Makes the object 'quiz' and inserts the question bank into the class
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
